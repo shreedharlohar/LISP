5e52e37299292f0b88ee3102621b29f8ef10812a
shreedharlohar <shreedhar122001@gmail.com>
2026-02-22 14:47:30 +0530

    interactive prompt v01

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello");
+static char input[2048];
+
+int main(int argc, char** argv){
+    puts("lispy version 0.0.0.1");
+    puts("Enter ctrl+c to exit \n");
+
+    while(1){
+        fputs("lipsy>",stdout);
+        fgets(input, 2048, stdin);
+        printf("No youre a %s", input);
+        if (input == "q"){
+            printf("got it");
+            break;
+        }
+    }
+
     return 0;
 }
